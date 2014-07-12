4224c7a70f6fd8fbc8d04f58414e59fb7cd4dd08
Tom Jakubowski <tom@crystae.net>
2014-07-11 17:43:45 -0700

    display: Sprite drawing

--- src/display.rs
+++ src/display.rs
@@ -1,4 +1,8 @@
+use std::fmt;
+
+static COLS: uint = 64;
 static ROWS: uint = 32;
+pub static MAX_SPRITE_SIZE: uint = 15;
 
 pub enum Pixel {
     On,
@@ -7,7 +11,7 @@ pub enum Pixel {
 
 impl Pixel {
     #[inline]
-    pub fn from_bool(b: bool) -> Pixel {
+    fn from_bool(b: bool) -> Pixel {
         if b { On } else { Off }
     }
 
@@ -47,8 +51,28 @@ impl Display {
         }
     }
 
+    pub fn draw(&mut self, sprite: &[u8], x: u8, y: u8) {
+        debug_assert!(sprite.len() <= MAX_SPRITE_SIZE);
+        let (x, mut y) = (x as uint % COLS, y as uint % ROWS);
+        for sprite in sprite.iter() {
+            let sprite: u64 = (*sprite as u64) << (64 - 8);
+            self.p[y] ^= sprite >> x;
+            if x != 0 { self.p[y] ^= sprite << (64 - x); }
+            y = (y + 1) % ROWS;
+        }
+    }
+
     pub fn clear(&mut self) {
         self.p = [0, ..ROWS]
+    }
+}
+
+impl fmt::Show for Display {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for &row in self.p.iter() {
+            try!(write!(f, "{:064t}\n", row));
+        }
+        Ok(())
     }
 }
 
@@ -98,5 +122,39 @@ mod test {
         d.p[0] = 943853945;
         d.clear();
         assert!(d.pixels().all(|x| x.is_off()));
+    }
+
+    #[test]
+    fn test_draw() {
+        let mut d = Display::new();
+        let sprite = [0b11111111];
+        d.draw(sprite.as_slice(), 0, 0);
+        assert!(d.pixels().take(8).all(|x| x.is_on()));
+        assert!(d.pixels().skip(8).all(|x| x.is_off()));
+        d.draw(sprite.as_slice(), 0, 0);
+        assert!(d.pixels().all(|x| x.is_off()));
+    }
+
+    #[test]
+    fn test_draw_wrapping_cols() {
+        use super::{COLS};
+        let mut d = Display::new();
+        let sprite = [0b11111111];
+        d.draw(sprite.as_slice(), COLS as u8 - 1, 0);
+        println!("{}", d);
+        assert!(d.pixels().take(7).all(|x| x.is_on()));
+        assert!(d.pixels().skip(63).take(1).all(|x| x.is_on()));
+        assert!(d.pixels().skip(7).take(56).all(|x| x.is_off()));
+    }
+
+    #[test]
+    fn test_draw_wrapping_rows() {
+        use super::{ROWS};
+        let mut d = Display::new();
+        let sprite = [0b11111111, 0b11111111];
+        d.draw(sprite.as_slice(), 0, ROWS as u8 - 1);
+        println!("{}", d);
+        assert!(d.pixels().take(8).all(|x| x.is_on()));
+        assert!(d.pixels().skip(64 * (ROWS - 1)).take(8).all(|x| x.is_on()));
     }
 }
